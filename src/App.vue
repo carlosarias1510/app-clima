4c46fedad8100a2c73d2f69f8e252e4c240833d1
carlosarias1510 <120199600+carlosarias1510@users.noreply.github.com>
2024-08-08 11:53:36 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,87 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div id="contenedor">
+      <div id="caja1">
+        <h1 id="temperatura-valor">{{ temperaturaValor }}</h1>
+        <h1 id="temperatura-descripcion">{{ temperaturaDescripcion }}</h1>
+      </div>
+      <div id="caja2">
+        <h2 id="ubicacion">{{ ubicacion }}</h2>
+        <img id="icono-animado" :src="iconoAnimado" alt="" height="128" width="128">
+      </div>
+      <div id="caja3">
+        <h3>Veloc. del Viento</h3>
+        <h1 id="viento-velocidad">{{ vientoVelocidad }}</h1>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import '@/assets/styles.css';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  data() {
+    return {
+      temperaturaValor: '',
+      temperaturaDescripcion: '',
+      ubicacion: '',
+      iconoAnimado: '',
+      vientoVelocidad: ''
+    };
+  },
+  mounted() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(posicion => {
+        let lon = posicion.coords.longitude;
+        let lat = posicion.coords.latitude;
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=adfdefbb82d8a2e392b7d1e15cf9e1f1&units=metric&lang=es`;
+
+        fetch(url)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+          })
+          .then(data => {
+            this.temperaturaValor = `${Math.round(data.main.temp)} °C`;
+            this.temperaturaDescripcion = data.weather[0].description.toUpperCase();
+            this.ubicacion = data.name;
+            this.vientoVelocidad = `${data.wind.speed} m/s`;
+
+            switch (data.weather[0].main) {
+              case 'Thunderstorm':
+                this.iconoAnimado = require('@/assets/animated/thunder.svg');
+                break;
+              case 'Drizzle':
+                this.iconoAnimado = require('@/assets/animated/rainy-2.svg');
+                break;
+              case 'Rain':
+                this.iconoAnimado = require('@/assets/animated/rainy-7.svg');
+                break;
+              case 'Snow':
+                this.iconoAnimado = require('@/assets/animated/snowy-6.svg');
+                break;
+              case 'Clear':
+                this.iconoAnimado = require('@/assets/animated/day.svg');
+                break;
+              case 'Atmosphere':
+                this.iconoAnimado = require('@/assets/animated/weather.svg');
+                break;
+              case 'Clouds':
+                this.iconoAnimado = require('@/assets/animated/cloudy-day-1.svg');
+                break;
+              default:
+                this.iconoAnimado = require('@/assets/animated/cloudy-day-1.svg');
+            }
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      });
+    }
+  }
+};
+</script>
